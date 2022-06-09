fde63bd973e94169519aae42fb86cb90d7c535c1
Javier Postigo Arévalo <a21poarja@iesgrancapitan.org>
2022-06-09 09:12:51 +0200

    docs: comentarios javadoc

--- src/com/JavierPostigoArevalo/examen/junio/bike.java
+++ src/com/JavierPostigoArevalo/examen/junio/bike.java
@@ -1,56 +1,108 @@
 package com.JavierPostigoArevalo.examen.junio;
 
 
-class bike implements vehicle {  
+/**
+ * Esta clase simula el comportamiento de una bicicleta
+ * @author Javier
+ *
+ */
+class bike implements vehicle { 
+	/**
+	 * Argumento speed.
+	 */
   private int speed;
+  
+  /**
+	 * Argumento gear.
+	 */
   private int gear;
     
+  /**
+   * Cambiamos el equipo.
+   * @param int newGear
+   * 
+   * @Override
+
+   */
   @Override
 public void changeGear(int newGear){     
       gear = newGear;
   }
     
-  
-  @Override
+/**
+ * Obtenemos la velocidad de la bicicleta.
+ * @Override
+ * @return speed
+ */
+@Override
 public int getSpeed() {
 	return speed;
 }
 
-
+/**
+ * Podemos establecer la velocidad a la que va la bicicleta.
+ * @Override
+ * @param int speed
+ */
 @Override
 public void setSpeed(int speed) {
 	this.speed = speed;
 }
 
 
-
+/**
+ * Aumentamos la velocidad de la bicicleta.
+ * @Override
+ * @param int increment
+ */
 @Override
 public void speedUp(int increment){
       speed = speed + increment;
   }
     
+/**
+ * Presionamos los frenos de la bicicleta.
+ * @Override
+ * @param int decrement
+ */
   @Override
 public void applyBrakes(int decrement){     
       speed = speed - decrement;
   }
-    
+  /**
+   * Imprimimos en la pantalla.
+   * @Override
+   */
   @Override
 public void printStates() {
       System.out.println("speed: " + speed
           + " gear: " + gear);
   }
 
-
+  /**
+   * Metodo toString.
+   * @Override
+   * @return "bike [speed=" + speed + ", gear=" + gear + "]"
+   */
 @Override
 public String toString() {
 	return "bike [speed=" + speed + ", gear=" + gear + "]";
 }
+/**
+ * Obtenemos el equipamiento(gear).
+ * @Override
+ * @return gear
+ */
 @Override
 public int getGear() {
 	return gear;
 }
 
-
+/**
+ * Establecemos el equipamiento(gear).
+ * @param int gear
+ * @Override
+ */
 @Override
 public void setGear(int gear) {
 	this.gear = gear;
--- src/com/JavierPostigoArevalo/examen/junio/vehicle.java
+++ src/com/JavierPostigoArevalo/examen/junio/vehicle.java
@@ -1,21 +1,43 @@
 package com.JavierPostigoArevalo.examen.junio;
 
+/**
+ * Esta clase de java es una interfaz.
+ * @author javie
+ *
+ */
 interface vehicle {
 
+	/**
+	 * @param int newGear
+	*/
 	void changeGear(int newGear);
-
+	/**
+	 * Obtener la velocidad del vehiculo
+	*/
 	int getSpeed();
-
+	/**
+	 * @param int speed
+	*/
+	
 	void setSpeed(int speed);
-
+	/**
+	 * @param int increment
+	*/
 	void speedUp(int increment);
-
+	/**
+	 * @param int decrement
+	*/
 	void applyBrakes(int decrement);
-
+	
 	void printStates();
-
+	/**
+	 *
+	*/
 	int getGear();
-
+	/**
+	 * @param int gear
+	 * 
+	*/
 	void setGear(int gear);
 
 }
